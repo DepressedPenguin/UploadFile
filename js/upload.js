88d0f19bb9f828e9372d6a5d19e988cfae8c99a9
DepressedPenguin <zakariaelaro@gmail.com>
2023-02-28 16:06:14 +0100

    my site

---
+++ js/upload.js
@@ -0,0 +1,12 @@
+function handlonclick(){
+	document.getElementById("paypal").innerHTML = "Paypal : ZakariaHyphen@gmail.com";
+	document.getElementById("cont-h1").innerHTML = "لغات";
+}
+
+function handlonclicklanguage(){
+	document.getElementById("cont-h1").innerHTML = "حمل ملفك الان؟";
+	document.getElementById("ann").innerHTML = "ملاحضة لا يمكننا الاستمرار بدون مال";
+	document.getElementById("li1").innerHTML = "حول";
+	document.getElementById("li2").innerHTML = "المساعدة";
+	document.getElementById("cont-p").innerHTML = "الحد الاقصئ لتحميل 1جيغا";
+}
